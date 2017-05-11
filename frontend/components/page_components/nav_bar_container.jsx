adfdd4148a565d1f22f672b090aaead3b475998b
Yuan <yuangao2.12@outlook.com>
2017-05-11 15:50:45 -0400

    Set up frontend for notification dropdown

--- frontend/components/page_components/nav_bar_container.jsx
+++ frontend/components/page_components/nav_bar_container.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
+import ClickOutHandler from 'react-onclickout';
+
 
 class NavBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      notificationIsOpen: false,
+    };
 
   this.redirectToSelfPage = this.redirectToSelfPage.bind(this);
   this.redirectToDiscover = this.redirectToDiscover.bind(this);
+  this.displayNotification = this.displayNotification.bind(this);
+  this.hideNotificaton = this.hideNotificaton.bind(this);
   }
 
   redirectToSelfPage() {
@@ -18,12 +25,40 @@ class NavBar extends React.Component {
     this.props.router.push('/discover');
   }
 
+  triangleClassName() {
+    return this.state.notificationIsOpen ? 'triangle' : 'triangle hidden';
+  }
+
+  notificationClassName() {
+    return this.state.notificationIsOpen ? 'notification' : 'notification hidden';
+  }
+
+  displayNotification() {
+    this.setState({notificationIsOpen: true});
+  }
+
+  hideNotificaton(e) {
+    if (e.target.className === 'heart-black') {
+      return;
+    }
+    this.setState({notificationIsOpen: false});
+  }
+
   render() {
     return (
-      <div>
+      <div className='nav'>
         <div className='discover' onClick={ this.redirectToDiscover }></div>
-        <div className='heart-black'></div>
-        <div className='self' onClick={this.redirectToSelfPage}></div>
+        <div className='heart-black' onClick={ this.displayNotification }/>
+        <div className='self' onClick={ this.redirectToSelfPage }></div>
+        <ClickOutHandler onClickOut={ this.hideNotificaton }>
+          <div className={this.triangleClassName()}/>
+          <div className={this.notificationClassName()}>
+            <div className='no-notification'>
+              <p>Recent Activity on your posts</p>
+              <p>When someone comments on or likes ones of your photos or videos, you'll see it here.</p>
+            </div>
+          </div>
+        </ClickOutHandler>
       </div>
     );
   }
